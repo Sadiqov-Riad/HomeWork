5c1128327a56cdc82074512d256d3d1b6841ab6a
Садыгов Риад Вугар <Sadyg_wn39@itstep.edu.az>
2025-07-15 18:16:08 +0400

    added react-project

---
+++ React/react-project/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-black text-white">
+      everything is working
+    </div>
+    </>
+  )
+}
+
+export default App
