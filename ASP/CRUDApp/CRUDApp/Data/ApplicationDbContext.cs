44fa896b297d13658aaf500eaa3fed92ad6e98ea
Садыгов Риад Вугар <sadyg_wn39@itstep.edu.az>
2025-09-13 17:57:53 +0400

    add asp mvc crud app

---
+++ ASP/CRUDApp/CRUDApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CRUDApp.Models;
+
+namespace CRUDApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; } = null!;
+    }
+}
